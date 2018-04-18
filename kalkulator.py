47e64757ce93f55a6e8717d52bc62dc428532969
Paulina Padniewska <46747@student.ue.poznan.pl>
2018-04-18 08:23:33 +0200

    funkcja z pomocą gotowa do review

--- kalkulator.py
+++ kalkulator.py
@@ -1,1 +1,10 @@
-print("Hello world")
+a=int(input('Podaj pierwsza liczbe')
+    b=int(input('Podaj druga liczbe')
+    print(a+b)
+    
+def dodaj get_help():
+    print('To jest prosty program kalkulatora. Wprowadü dwie liczby i zatwierdü enterem.')
+    
+    
+get_help()
+dodaj()
