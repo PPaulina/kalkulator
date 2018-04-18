a227a58b2211af187b95c20011179e0f3f876a5b
Paulina Padniewska <46747@student.ue.poznan.pl>
2018-04-18 08:31:10 +0200

    rozgrzebana funkcja dodwania

--- kalkulator.py
+++ kalkulator.py
@@ -1,10 +1,8 @@
-a=int(input('Podaj pierwsza liczbe')
-    b=int(input('Podaj druga liczbe')
-    print(a+b)
-    
-def dodaj get_help():
-    print('To jest prosty program kalkulatora. Wprowadü dwie liczby i zatwierdü enterem.')
-    
-    
-get_help()
-dodaj()
+-def dodaj():
+-	a = int(input('Podaj pierwsza liczbe'))
+-	b = int(input('Podaj druga liczbe'))
+-	print(a+b)
++def dodaj(a, b):
++	wynik = a + b
+ 	
+ dodawanie()
